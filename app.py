950b2e316bded6ea5b81b816c4bf74ff1422b78f
kino14n <disenokinocomp@gmail.com>
2025-07-18 12:20:51 -0500

    servidor

--- app.py
+++ app.py
@@ -141,6 +141,3 @@ def descargar(filename):
         return redirect(url_for('index'))
     return send_file(path, as_attachment=True)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
